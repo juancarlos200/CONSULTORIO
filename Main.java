0108814bd8ce539d4275ded115c20487f57999b8
juancarlos200 <jk6610687@gmai.com>
2025-09-01 19:34:44 -0600

    dsdad

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ public class Main {
         // Ejemplo de uso de las clases
 
         // Crear un encargado
-        Encargado encargado = new Encargado("Carlos Pérez", 12345678, "carlos@example.com");
+        Encargado encargado = new Encargado("Carlos Pérez", 12345678, "carlos@gmail.com");
 
         // Crear una mascota
         Mascota mascota = new Mascota("Firulais", "Labrador", "Perro", 5);
 
         // Crear una consulta
-        Consulta consulta = new Consulta("Chequeo general", 20250801, 25.3f, "Vitaminas y dieta balanceada");
+        Consulta consulta = new Consulta("Chequeo general", 20/05/2025, 25f, "Vitaminas y dieta balanceada");
 
         // Crear un reporte
         Reporte reporte = new Reporte(12);
