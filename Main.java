3d59fb19904f4e30823335e938de737bb5e8b9cb
juancarlos200 <jk6610687@gmai.com>
2025-09-01 19:31:19 -0600

    cambie el main

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        // Ejemplo de uso de las clases
+
+        // Crear un encargado
+        Encargado encargado = new Encargado("Carlos Pérez", 12345678, "carlos@example.com");
+
+        // Crear una mascota
+        Mascota mascota = new Mascota("Firulais", "Labrador", "Perro", 5);
+
+        // Crear una consulta
+        Consulta consulta = new Consulta("Chequeo general", 20250801, 25.3f, "Vitaminas y dieta balanceada");
+
+        // Crear un reporte
+        Reporte reporte = new Reporte(12);
+
+        // Mostrar información
+        System.out.println("Encargado: " + encargado.getNombreCompleto());
+        System.out.println("Mascota: " + mascota.getNombre() + " (" + mascota.getEspecie() + ")");
+        System.out.println("Consulta: " + consulta.getMotivoConsulta() + " - Tratamiento: " + consulta.getTratamientoPrescrito());
+        System.out.println("Consultas este mes: " + reporte.getCantidadMensual());
+    }
 }
